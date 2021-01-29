f0cea65f543e04adda854aef9cf2f92eec06176f
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-01-29 14:25:13 -0800

    add start script

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require("helmet");
-const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/petfinder';
+//process.env.DB_URL||
+const dbUrl='mongodb://localhost:27017/petfinder';
 const MongoDBStore=require("connect-mongo")(session);
 
 // Requiring schema.
@@ -167,6 +168,7 @@ app.use((err, req, res, next) => {
 });
 
 // Set up Show page to 'show.ejs'
-app.listen(4000, () => {
-    console.log('Serving on port 4000')
+const port=process.env.PORT||4000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
