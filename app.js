4c154021c7f2c6abba012e0f313c126e0e7ebc5d
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-01-29 18:06:36 -0800

    Update Mongo URL

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require("helmet");
-//process.env.DB_URL||
-const dbUrl='mongodb://localhost:27017/petfinder';
+const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/petfinder';
 const MongoDBStore=require("connect-mongo")(session);
 
 // Requiring schema.
